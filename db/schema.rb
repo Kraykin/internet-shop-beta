c9d3eb6350f1410ccb4bf90cf9de8502722cea7c
Sergey Kraykin <kraykin@gmail.com>
2016-03-16 22:32:05 +0300

    Run 'rake db:migrate'

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160316150142) do
+ActiveRecord::Schema.define(version: 20160316191941) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
