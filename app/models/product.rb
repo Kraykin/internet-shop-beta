a524d96fe1188492e9f1e02914cb650be69c00cf
Sergey Kraykin <kraykin@gmail.com>
2016-03-17 12:58:14 +0300

    Add validation to product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,12 +4,13 @@ class Product < ActiveRecord::Base
 
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
-  validates :title, uniqueness: true
+  validates :title, :image_url, uniqueness: true
   validates :image_url, allow_blank: true, format: {
     with:    %r{\.(gif|jpg|png)\Z}i,
     message: 'must be a URL for GIF, JPG or PNG image.'
   }
   validates :title, length: { minimum: 10 }
+  validates :price, :numericality => {:less_than_or_equal_to => 1000}
 
   def self.latest
     Product.order(:updated_at).last
