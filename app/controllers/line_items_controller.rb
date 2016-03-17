d5976794627b9263e11625a7e5fcc894610aec33
Sergey Kraykin <kraykin@gmail.com>
2016-03-17 22:33:11 +0300

    Add link to decrement the quantity of the item

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,6 +1,6 @@
 class LineItemsController < ApplicationController
   include CurrentCart
-  before_action :set_cart, only: [:create]
+  before_action :set_cart, only: [:create, :decrease]
   before_action :set_line_item, only: [:show, :edit, :update, :destroy]
 
   # GET /line_items
@@ -65,6 +65,23 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       format.html {redirect_to store_path}
       format.json  { head :no_content }
+    end
+  end
+
+  # PUT /line_items/1
+  # PUT /line_items/1.json
+  def decrease
+    @line_item = @cart.decrease(params[:id])
+
+    respond_to do |format|
+      if @line_item.save
+        format.html { redirect_to store_path, notice: 'Line item was successfully updated.' }
+        format.js   { @current_item = @line_item }
+        format.json { head :ok }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @line_item.errors, status: :unprocessable_entity }
+      end
     end
   end
 
