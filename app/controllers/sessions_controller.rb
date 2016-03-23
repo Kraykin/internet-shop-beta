3a1c151b40d3f733758e544258b88d28b4fcfdda
Sergey Kraykin <kraykin@gmail.com>
2016-03-23 10:36:03 +0300

    Create a new user, if there is none

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,12 +5,18 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(name: params[:name])
-    if user and user.authenticate(params[:password])
+    if User.count > 1
+      user = User.find_by(name: params[:name])
+      if user and user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect_to admin_url
+      else
+        redirect_to login_url, alert: "Invalid user/password combination"
+      end
+    else
+      user = User.create(name: params[:name], password: params[:password])
       session[:user_id] = user.id
       redirect_to admin_url
-    else
-      redirect_to login_url, alert: "Invalid user/password combination"
     end
   end
 
