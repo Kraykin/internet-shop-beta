19927dd6118d8b6106a123857ff83847740d69c8
Sergey Kraykin <kraykin@gmail.com>
2016-03-24 14:56:07 +0300

    Add locale selector

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -3,8 +3,12 @@ class StoreController < ApplicationController
   include CurrentCart
   before_action :set_cart
   def index
+    if params[:set_locale]
+      redirect_to store_url(locale: params[:set_locale])
+    else
+      @products = Product.order(:title)
+    end
     session[:counter] ||= 0
     @count = session[:counter] += 1
-    @products = Product.order(:title)
   end
 end
